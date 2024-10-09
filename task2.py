ff74f856b4d20699363e17a17b5bc378aaa67702
Seán Kelly <s.kelly178@universityofgalway.ie>
2024-10-09 15:08:27 +0100

    Task 2 - Random Number Generator

--- task2.py
+++ task2.py
@@ -1,6 +1,5 @@
 import random
 
-
 class RandomNumberGenerator():
     
     def __init__(self, seed, lower_bound, upper_bound):
@@ -9,11 +8,10 @@ class RandomNumberGenerator():
         self.upper_bound = upper_bound
         
     def random_int(self):
-        # Your code goes here
+        return random.randint(self.lower_bound, self.upper_bound)
     
     def random_float(self):
-        # Your code goes here
-    
+        return random.uniform(self.lower_bound, self.upper_bound)
     
 if __name__ == '__main__':
     random_number_generator = RandomNumberGenerator(seed=10, lower_bound=0, upper_bound=100)
