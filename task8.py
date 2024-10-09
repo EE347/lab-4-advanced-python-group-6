098e1643558eb9c31359fec87dcd89b19cd20844
Seán Kelly <s.kelly178@universityofgalway.ie>
2024-10-09 15:30:16 +0100

    Task 8 - Graphing Sine and Cosine with Legend

--- task8.py
+++ task8.py
@@ -1,2 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 101)
+
+sin = np.load("task7_sin.npy")
+cos = np.load("task7_cos.npy")
+
+plt.plot(x, sin, color='blue', label='sin(x)')
+plt.plot(x, cos, color='orange', label='cos(x)')
+plt.legend(loc='lower left')
+plt.show()
