61740f845033c0f1cc353b182ea348e33d63914c
Seán Kelly <s.kelly178@universityofgalway.ie>
2024-10-09 15:18:23 +0100

    Task 5 - Write to and Read from CSV

--- task5.py
+++ task5.py
@@ -1,2 +1,13 @@
 import csv
 
+with open("task5.csv", "w+") as f:
+    while True:
+        name = input("What is your name? (or quit) ")
+        if name == "quit":
+            break
+        else:
+            f.write(f"{name}\n")
+    
+    f.seek(0)
+    for line in f.readlines():
+        print(line)
